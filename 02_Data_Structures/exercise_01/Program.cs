b07a651e5745db7df51dae63ed694f1a6a3cf899
Axlfc <axelfernandezcurros@gmail.com>
2023-03-22 13:14:26 +0100

    ADDED: 02_Data_Structures

---
+++ 02_Data_Structures/exercise_01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a data structure for a customer with these fields:
+            // - Full name
+            // - Phone
+            // - Address
+            // - E-mail
+            // - If you are a new customer
+
+            // Bonus: write a method to render this data from the structure by doing Console.WriteLine(...)
+
+
+        }
+    }
+
+}
