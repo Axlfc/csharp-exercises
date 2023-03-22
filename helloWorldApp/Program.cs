48a8c65fb58e3b5653c74882636cd7573207f15e
Axlfc <axelfernandezcurros@gmail.com>
2023-03-22 12:17:37 +0100

    UPDATED: helloWorldApp

--- helloWorldApp/Program.cs
+++ helloWorldApp/Program.cs
@@ -6,9 +6,16 @@ namespace app
     {
         static void Main(string[] args)
         {
+            // Get current Date & Time
             DateTime dt = DateTime.Now;
-            Console.WriteLine("Hello World!!!");
-            Console.WriteLine(dt.ToString());
+
+            // Ask the user for their name
+            Console.WriteLine("Write your name:");
+            string name = Console.ReadLine();
+
+            // Show messages
+            Console.WriteLine("Hello " + name + "!!!");
+            Console.WriteLine("Today is " + dt.ToString());
         }
     }
 
