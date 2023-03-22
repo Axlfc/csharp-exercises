78c0c59c7417422b2c2a81787b3a5365a059f3ef
Axlfc <axelfernandezcurros@gmail.com>
2023-03-22 14:52:24 +0100

    ADDED: exercises 04_01 04_02

---
+++ 04_Loops_II/exercise_01/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace app {
+    class Program {
+        static void Main(string[] args) {
+            // Write a program that:
+
+            // Request data from a customer: Name, email, coupon
+            // Determine if a customer has a discount coupon
+            // Show a discounted price based on the discount
+            // Show the price of a product without discount if there is no coupon
+            // Note: you can set a fixed price for a product or a variable one.
+
+            // Set the product price
+            double productPrice = 50.00;
+            string? customerName = null;
+            string? customerEmail = null;
+            string? couponCode = null;
+
+            // Request customer information
+            Console.Write("Enter your name: ");
+            customerName = Console.ReadLine();
+
+            Console.Write("Enter your email: ");
+            customerEmail = Console.ReadLine();
+
+            Console.Write("Enter your coupon code (if you have one): ");
+            couponCode = Console.ReadLine();
+
+            if (customerName != null && customerEmail != null && couponCode != null) {
+                // Check if customer has a valid coupon code
+                bool hasDiscountCoupon = false;
+                double discountPercentage = 0.0;
+
+                if (couponCode == "DISCOUNT10") {
+                    hasDiscountCoupon = true;
+                    discountPercentage = 0.10;
+                }
+
+                // Calculate the discounted price if customer has a valid coupon
+                double discountedPrice = 0.0;
+
+                if (hasDiscountCoupon) {
+                    discountedPrice = productPrice - (productPrice * discountPercentage);
+                    Console.WriteLine($"Dear {customerName}, your discounted price is: ${discountedPrice:F2}");
+                } else {
+                    Console.WriteLine($"Dear {customerName}, the price of the product without discount is: ${productPrice:F2}");
+                }
+            }
+        }
+    }
+}
---
+++ 04_Loops_II/exercise_02/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ProgrammingLanguages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Make a list of programming languages, for example: C#, Java, C++. Presents the list to the console and asks the user to select the language using 1, 2, 3 or a, b, c. Present the result in console.
+
+            // Note: you can add “Hello world” to the result for C#.
+
+            List<string> languages = new List<string> {"Python", "Bash", "Powershell", "Dart", "Java", "C#", "C++", "C"};
+
+            // Display list of programming languages to console
+            Console.WriteLine("Select a programming language:");
+            for (int i = 0; i < languages.Count; i++) {
+                Console.WriteLine($"{i+1}. {languages[i]}");
+            }
+
+            // Prompt user to select language
+            Console.Write("Selection: ");
+            string input = Console.ReadLine().ToLower();
+
+            switch(input) {
+                case "1":
+                case "a":
+                    Console.WriteLine("You selected Python");
+                    break;
+                case "2":
+                case "b":
+                    Console.WriteLine("You selected Bash");
+                    break;
+                case "3":
+                case "c":
+                    Console.WriteLine("You selected Powershell");
+                    break;
+                case "4":
+                case "d":
+                    Console.WriteLine("You selected Dart");
+                    break;
+                case "5":
+                case "e":
+                    Console.WriteLine("You selected Java");
+                    break;
+                case "6":
+                case "f":
+                    Console.WriteLine("Hello, world");
+                    break;
+                case "7":
+                case "g":
+                    Console.WriteLine("You selected C++");
+                    break;
+                case "8":
+                case "h":
+                    Console.WriteLine("You selected C");
+                    break;
+            }
+        }
+    }
+}
