0949f94cd0a98b39aea20c38d7c217414cf1a610
Axlfc <axelfernandezcurros@gmail.com>
2023-03-22 11:00:40 +0100

    ADDED: Hello World C# project

---
+++ app/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = DateTime.Now;
+            Console.WriteLine("Hello World");
+            Console.WriteLine(dt.ToString());
+        }
+    }
+
+}
+
+// show current date in C#?
