b6b65194fc77c95947edd00c4c5ff3b82c5e5e5b
Axlfc <axelfernandezcurros@gmail.com>
2023-03-22 12:32:21 +0100

    ADDED: exercises 01, 02, 03 Variables Types and Operators

---
+++ 01_Variables_Types_and_Operators/exercise_01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program that receives data from a person and generates a message, use a variable for each data and another for the message. 
+            // Eg: number, last name, age, knows how to program, etc.
+
+            // Ask the user for their name
+            Console.WriteLine("Write your name:");
+            string name = Console.ReadLine();
+
+            // Ask the user for their last name
+            Console.WriteLine("Write your last name:");
+            string last_name = Console.ReadLine();
+
+            // Ask the user for their name
+            Console.WriteLine("Write your age:");
+            string age = Console.ReadLine();
+
+            // Ask the user if they know how to program
+            Console.WriteLine("Do you know how to program?");
+            string know_to_program = Console.ReadLine();
+
+            // Show messages
+        }
+    }
+
+}
