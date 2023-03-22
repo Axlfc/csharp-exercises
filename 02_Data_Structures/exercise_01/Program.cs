2903a3de7537bc81466faaec882c7519c68723a7
Axlfc <axelfernandezcurros@gmail.com>
2023-03-22 13:21:20 +0100

    UPDATED: Exercise 02_01

--- 02_Data_Structures/exercise_01/Program.cs
+++ 02_Data_Structures/exercise_01/Program.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 
-namespace app
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace app {
+    class Program {
+        public struct Customer {
+            public string FullName;
+            public string Phone;
+            public string Address;
+            public string Email;
+            public bool IsNewCustomer;
+
+            public void Render() {
+                Console.WriteLine("Full Name: " + FullName);
+                Console.WriteLine("Phone: " + Phone);
+                Console.WriteLine("Address: " + Address);
+                Console.WriteLine("Email: " + Email);
+                Console.WriteLine("New Customer: " + IsNewCustomer);
+            }
+        }
+
+
+        static void Main(string[] args) {
             // Create a data structure for a customer with these fields:
             // - Full name
             // - Phone
@@ -15,7 +29,16 @@ namespace app
 
             // Bonus: write a method to render this data from the structure by doing Console.WriteLine(...)
 
+            Customer customer = new Customer
+            {
+                FullName = "John Smith",
+                Phone = "555-1234",
+                Address = "123 Main St.",
+                Email = "john.smith@example.com",
+                IsNewCustomer = true
+            };
 
+            customer.Render();
         }
     }
 
