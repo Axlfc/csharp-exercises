2e375310b2616c909179d870060490d10ff3f6aa
Axlfc <axelfernandezcurros@gmail.com>
2023-03-22 13:08:40 +0100

    UPDATED: 01_03

--- 01_Variables_Types_and_Operators/exercise_03/Program.cs
+++ 01_Variables_Types_and_Operators/exercise_03/Program.cs
@@ -13,6 +13,31 @@ namespace app
             // - One of two conditions is true at the same time (one true and one false)
 
             // Note: you can write this data by console if you prefer to see it. The idea of the exercise is to store the data in the most suitable types.
+
+
+            // Operators
+            // Arithmetic: +, -, *, /
+            // Autoincrement ++, Autodecrement --
+
+            // Assignation: +=, -=, *=, etc.
+
+            // Comparation: < > <= >= == !=
+
+            // Logical: &&, ||, !
+
+            int n = 22;
+            char letter = 'a';
+
+            bool condition_1 = (n>=18);
+            bool condition_2 = (letter=='a');
+            bool condition_3 = (condition_1 && condition_2);
+            bool condition_4 = (condition_3 || false);
+
+            Console.WriteLine("Number is greater than or equal to 18 = " + condition_1);
+            Console.WriteLine("A char is 'a' = " + condition_2);
+            Console.WriteLine("Two conditions are fulfilled at the same time (both true) = " + condition_3);
+            Console.WriteLine("One of two conditions is true at the same time (one true and one false) = " + condition_4);
+
         }
     }
 
