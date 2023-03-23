2be8341dd2c4cb348e1df028a1431747a22dd5ac
Axlfc <axelfernandezcurros@gmail.com>
2023-03-23 15:52:30 +0100

    UPDATED: 10

--- 09_Classes_and_Objects_II_and_LINQ/Program.cs
+++ 09_Classes_and_Objects_II_and_LINQ/Program.cs
@@ -1,9 +1,39 @@
-﻿using System;
+﻿// LINQ
 
-namespace ClassesAndObjectsAndLINQ {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World");
-        }
-    }
+// Origen de datos
+int [] numbers = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+// Obtener datos
+var pares =
+    from numero in numbers
+    where (numero % 2) == 0
+    select numero;
+
+var impares =
+    from numero in numbers
+    where (numero % 2) != 0
+    select numero;
+
+var nums =
+    from numero in numbers  
+    where (numero > 5 && numero <= 8)
+    select numero;
+
+// Ejecutar consulta
+
+foreach (var num in pares) {
+    Console.Write(num + " ");
 }
+Console.WriteLine(" ");
+foreach (var num in impares) {
+    Console.Write(num + " ");
+}
+Console.WriteLine(" ");
+foreach (var num in nums) {
+    Console.Write(num + " ");
+}
+
+
+// Utilizar en nuestro programa
+
+// C#
--- 10_Classes_and_Objects_III/Program.cs
+++ 10_Classes_and_Objects_III/Program.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿// Generics
+Generic<string> str = new Generic<string>();
+str.Dato = "dato";
 
-namespace ClasesAndObjects {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World");
-        }
-    }
+Console.WriteLine($"Generic Dato: {str.Dato}");
+Console.WriteLine($"Tipo de Generic Dato: {str.Dato.GetType()}");
+Console.WriteLine($"Tipo de  Generic Dato: {str.GetType()}");
+
+
+public class Generic<T> {
+    public T Dato { get; set; }
 }
